48a35f625aa1c4c174cee4379382d4534aeec346
Sebastian Krzyszkowiak <s.krzyszkowiak@cogision.com>
2014-02-19 14:39:13 +0100

    use event handler deregistering by reference instead of unique namespace

--- angular-konami.js
+++ angular-konami.js
@@ -1,4 +1,4 @@
-// angular-konami 0.1
+// angular-konami 0.2
 // https://github.com/dos1/angular-konami
 // based on https://gist.github.com/benajnim/5238495
 angular.module('konami', []).directive("konami", ['$document', function($document) {
@@ -8,24 +8,24 @@ angular.module('konami', []).directive("konami", ['$document', function($documen
                 konami: '&'
             },
             link: function(scope) {
-                var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-                var konami_index = 0;
-                var rand = Date.now() + "-" + Math.round(Math.random() * 1000); // unique event name to avoid collisions when using the directive on multiple elements
-                $($document).on('keydown.konami' + rand, function(e) {
+                var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], konami_index = 0;
+
+                var handler = function(e) {
                     if (e.keyCode === konami_keys[konami_index++]) {
                         if (konami_index === konami_keys.length) {
-                            $($document).off('keydown.konami' + rand);
+                            $document.off('keydown', handler);
                             scope.$apply(scope.konami);
                         }
                     } else {
                         konami_index = 0;
                     }
-                });
+                };
+
+                $document.on('keydown', handler);
 
                 scope.$on('$destroy', function() {
-                    $($document).off('keydown.konami' + rand);
+                    $document.off('keydown', handler);
                 });
             }
         };
-    }
-]);
+    }]);
